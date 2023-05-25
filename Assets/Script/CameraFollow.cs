68cf6a5fc357273506ea753dd23d12aa28e2498f
Zhu Minghui <110764547+ZMinghuiZ@users.noreply.github.com>
2023-05-25 12:32:05 +0800

    Add initial project asset

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform Player;
+
+    private Vector3 tempPos;
+
+    [SerializeField]
+    private float minX, minY, maxX, maxY;
+    
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        tempPos.x = Player.position.x;
+        tempPos.y = Player.position.y;
+
+        if (tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+
+        if (tempPos.y < minY)
+        {
+            tempPos.y = minY;
+        }
+        if (tempPos.y > maxY)
+        {
+            tempPos.y = maxY;
+        }
+
+        transform.position = tempPos;
+        
+    }
+}
